09d5fdfa7516baeddd09622b7c80f2d4fc4b0931
AYUSH YADAV <ayushexx@gmail.com>
2021-12-07 14:10:52 +0530

    feat: footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialIconLink,
+  SocialIcons,
+  SocialLogo,
+  WebsiteRights,
+} from "./FooterElements";
+import {
+  FaInstagram,
+  FaFacebook,
+  FaYoutube,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <>
+      <FooterContainer>
+        <FooterWrap>
+          <FooterLinksContainer>
+            <FooterLinksWrapper>
+              <FooterLinkItems>
+                <FooterLinkTitle>About us</FooterLinkTitle>
+                <FooterLink to="/signin">How it works</FooterLink>
+                <FooterLink to="/signin">Testimonials</FooterLink>
+                <FooterLink to="/signin">Careers</FooterLink>
+                <FooterLink to="/signin">Investors</FooterLink>
+                <FooterLink to="/signin">Terms of service</FooterLink>
+              </FooterLinkItems>
+              <FooterLinkItems>
+                <FooterLinkTitle>Contact us</FooterLinkTitle>
+                <FooterLink to="/">Contact</FooterLink>
+                <FooterLink to="/">Support</FooterLink>
+                <FooterLink to="/">Destinations</FooterLink>
+                <FooterLink to="/">Sponsorships</FooterLink>
+              </FooterLinkItems>
+            </FooterLinksWrapper>
+            <FooterLinksWrapper>
+              <FooterLinkItems>
+                <FooterLinkTitle>Videos</FooterLinkTitle>
+                <FooterLink to="/">Submit video</FooterLink>
+                <FooterLink to="/">Ambassadors</FooterLink>
+                <FooterLink to="/">Agency</FooterLink>
+                <FooterLink to="/">Influencers</FooterLink>
+              </FooterLinkItems>
+              <FooterLinkItems>
+                <FooterLinkTitle>Social media</FooterLinkTitle>
+                <FooterLink to="/">Instagram</FooterLink>
+                <FooterLink to="/">Facebook</FooterLink>
+                <FooterLink to="/">Youtube</FooterLink>
+                <FooterLink to="/">Twitter</FooterLink>
+              </FooterLinkItems>
+            </FooterLinksWrapper>
+          </FooterLinksContainer>
+
+          <SocialMedia>
+            <SocialMediaWrap>
+              <SocialLogo to="/">virtua</SocialLogo>
+              <WebsiteRights>
+                virtua {new Date().getFullYear()} All rights reserved.
+              </WebsiteRights>
+              <SocialIcons>
+                <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                  <FaInstagram />
+                </SocialIconLink>
+                <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                  <FaFacebook />
+                </SocialIconLink>
+                <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                  <FaYoutube />
+                </SocialIconLink>
+                <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                  <FaTwitter />
+                </SocialIconLink>
+                <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                  <FaLinkedin />
+                </SocialIconLink>
+              </SocialIcons>
+            </SocialMediaWrap>
+          </SocialMedia>
+        </FooterWrap>
+      </FooterContainer>
+    </>
+  );
+}
+
+export default Footer;
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SignInPage() {
     return (
         <>
-            Sign in
+            {/* Sign in */}
         </>
     )
 }
